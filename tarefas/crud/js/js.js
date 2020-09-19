f7293f2cdebe554d44ed94b8b2eb07e25f5e48f7
luiza <luiza_andres@uol.com.br>
2020-09-18 22:30:00 -0300

    crud js

---
+++ tarefas/crud/js/js.js
@@ -0,0 +1,35 @@
+var receita1 = {
+    nomePaciente: "Leonardo",
+    medicamento: 30,
+    dose: "987654",
+    intervalo: "Senac",
+    duracao: "Senac"
+};
+
+var receita2 = {
+    nomePaciente: "Leonardo",
+    medicamento: 30,
+    dose: "987654",
+    intervalo: "Senac",
+    duracao: "Senac"
+};
+
+function adicionar(){
+    //html injection pra criar nova linha create element da aula passada
+    //divBox.innerHTML = "<p>"+valorNome+"</p>";
+    //pega a info do nome
+    var nome = document.getElementById('nomePaciente').value;
+    //seleciona o elemento alvo
+    var novo = document.getElementById('celulaNomePaciente');
+    //atribui valor seleionado no alvo
+    celulaNomePaciente.textContent=nomePaciente;
+}
+
+function excluir(id){
+    
+}
+
+function limparConteudo(){
+    var limparInputNome = document.getElementById("nomePaciente");
+    limparInputNome.value = "";
+}
